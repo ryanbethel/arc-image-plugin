1bd2c81b853dadd7f6dc51d4443337ec781332e4
Ryan Bethel <ryan.bethel@gmail.com>
2021-11-05 23:10:16 -0400

    yup

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ let path = require('path')
 function package ({ arc, cloudformation }) {
 
 
-  let appName = arc.app
-  let transformSrc = path.resolve(__dirname, './src')
+  // let appName = arc.app
+  // let transformSrc = path.resolve(__dirname, './src')
 
+  // if (cloudformation.Resources.HTTP.paths['/transform/{proxy+}']) {
 
-  // if (!cloudformation.HTTP.paths['/transform/{proxy+}']) {
-
-  //   cloudformation.HTTP.paths['/transform/{proxy+}'] = {
+  //   cloudformation.Resources.HTTP.paths['/transform/{proxy+}'] = {
   //     'get': {
   //       'x-amazon-apigateway-integration': {
   //         'payloadFormatVersion': '2.0',
@@ -83,7 +82,7 @@ function package ({ arc, cloudformation }) {
 
 
   // user must add the "get /transform/*" route to manifest file
-  if (cloudformation.HTTP.paths['/transform/{proxy+}']) {
+  if (cloudformation.Resources.HTTP.Properties.DefinitionBody.paths['/transform/{proxy+}']) {
 
     cloudformation.Resources['PrivateStorageMacroPolicy'] = {
       'Type': 'AWS::IAM::Policy',
@@ -179,12 +178,24 @@ function package ({ arc, cloudformation }) {
 
 let os = require('os')
 let fs = require('fs')
-function sandbox (){
-  return {
-    start: function (){
-      process.env.ARC_SANDBOX_IMAGE_CACHE_FOLDER = fs.mkdtempSync(path.join(os.tmpdir(), 'arc-img-cache'))
-    }
+let sandbox = {
+  start: function ( { arc, inventory, services },  callback) {
+    console.log('in variables')
+    console.log(inventory)
+    callback()
   }
+}
+function variables  ({ arc, cloudformation, stage, inventory }) {
+  const isLocal = stage === 'testing' // stage will equal 'testing' when running in sandbox, otherwise will be one of 'staging' or 'production' when running in a `deploy` context
+  let cacheBucket
+  let staticDir
+  if (isLocal) {
+    cacheBucket = fs.mkdtempSync(path.join(os.tmpdir(), 'arc-img-cache'))
+    staticDir = inventory.inv._project.src + '/' + inventory.inv.static.folder
+
+    return { cacheBucket, staticDir }
+  }
+  else return { cacheBucket: { 'Ref': 'ImageCacheBucket' } }
 }
 
 
@@ -194,161 +205,161 @@ let arc = require('@architect/functions')
 let Jimp = require('jimp')
 let sizeOf = require('image-size')
 let aws = require('aws-sdk')
-let fs = require('fs')
+// let fs = require('fs')
 // let path = require('path')
 let { createHash } = require('crypto')
 
-function imageHandler (req){
+async function imageHandler (req){
+  const discovery = await arc.services()
   let Region = process.env.AWS_REGION
+  let cacheBucket = discovery['image-plugin'].cacheBucket
+  let localStaticDir = discovery['image-plugin'].staticDir
 
-
-  return  arc.http.async(validate, checkCache, transform)
-
-  async function validate (req){
   // verify the request is properly formed
-    let rawPath = req.rawPath
-    let imagePath = rawPath.replace(/^\/transform\//i, '')
-    let rawQuery = req.rawQueryString
-    let hash = createHash('sha256')
-    hash.update(`${imagePath}?${rawQuery}`)
-    let queryFingerprint =  hash.digest('hex').slice(0, 10)
-    let ext = path.extname(imagePath).slice(1)
-    if (!(ext === 'jpg' || ext === 'png')) return { statusCode: 404 }
-    req.queryFingerprint = queryFingerprint
-    req.image = { path: imagePath, ext, mime: `image/${ext}` }
-  }
+  let rawPath = req.rawPath
+  let imagePath = rawPath.replace(/^\/transform\//i, '')
+  let rawQuery = req.rawQueryString
+  let hash = createHash('sha256')
+  hash.update(`${imagePath}?${rawQuery}`)
+  let queryFingerprint =  hash.digest('hex').slice(0, 10)
+  let ext = path.extname(imagePath).slice(1)
+  if (!(ext === 'jpg' || ext === 'png')) return { statusCode: 404 }
+  req.queryFingerprint = queryFingerprint
+  req.image = { path: imagePath, ext, mime: `image/${ext}` }
 
-  async function checkCache (req){
-    let queryFingerprint = req.queryFingerprint
-    let ext = req.image.ext
-    let mime = req.image.mime
-    let discovery = await arc.services()
-    let s3 = new aws.S3({ Region })
+  // check cache
+  // let queryFingerprint = req.queryFingerprint
+  // let ext = req.image.ext
+  let mime = req.image.mime
+  // let discovery = await arc.services()
+  let s3 = new aws.S3({ Region })
 
-    let buffer
-    let env = process.env.ARC_ENV || process.env.NODE_ENV
-    let isLive = (env === 'staging' || env === 'production')
+  let buffer
+  let env = process.env.ARC_ENV || process.env.NODE_ENV
+  let isLive = (env === 'staging' || env === 'production')
 
-    let exists = true
-    if (isLive) {
+  let exists = true
+  if (isLive) {
     // read from s3
-      let Bucket = discovery['storage-private']['image-cache']
-      let Key = `${queryFingerprint}.${ext}`
-      try {
-        let result = await s3.getObject({ Bucket, Key, }).promise()
-        buffer = result.Body
-      }
-      catch (e){
-        console.log(e)
-        exists = false
-      }
+    // let Bucket = discovery['storage-private']['image-cache']
+    let Bucket = cacheBucket
+    let Key = `${queryFingerprint}.${ext}`
+    try {
+      let result = await s3.getObject({ Bucket, Key, }).promise()
+      buffer = result.Body
     }
-    else {
+    catch (e){
+      console.log(e)
+      exists = false
+    }
+  }
+  else {
     // read from local filesystem
-      let pathToCache = path.join(__dirname, '..', '..', '..', 'tmp-cache')
-      let pathToFile = path.join(pathToCache, `${queryFingerprint}.${ext}`)
-      try {
-        buffer = fs.readFileSync(pathToFile)
-      }
-      catch (e){
-        console.log(e)
-        exists = false
-      }
+    // let pathToCache = path.join(__dirname, '..', '..', '..', 'tmp-cache')
+    let pathToFile = path.join(cacheBucket, `${queryFingerprint}.${ext}`)
+    // let pathToFile = path.join(pathToCache, `${queryFingerprint}.${ext}`)
+    try {
+      buffer = fs.readFileSync(pathToFile)
     }
-
-    if (exists) {
-      return {
-        statusCode: 200,
-        headers: { 'Content-Type': mime, isBase64Encoded: true },
-        body: buffer.toString('base64')
-      }
+    catch (e){
+      console.log(e)
+      exists = false
     }
   }
 
-  async function transform (req){
+  if (exists) {
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': mime, isBase64Encoded: true },
+      body: buffer.toString('base64')
+    }
+  }
+
+  // Transform
   // 1. first get the original image
-    let imagePath = req.image.path
-    console.log(imagePath)
-    let query = req.queryStringParameters
-    let height = query.height
-    let width = query.width
-    let queryFingerprint = req.queryFingerprint
-    let ext = req.image.ext
-    let mime = req.image.mime
-    let discovery = await arc.services()
-    let s3 = new aws.S3({ Region })
+  // let imagePath = req.image.path
+  console.log(imagePath)
+  let query = req.queryStringParameters
+  let height = query.height
+  let width = query.width
+  // let queryFingerprint = req.queryFingerprint
+  // let ext = req.image.ext
+  // let mime = req.image.mime
+  // let discovery = await arc.services()
+  // let s3 = new aws.S3({ Region })
 
-    let buffer
-    let env = process.env.ARC_ENV || process.env.NODE_ENV
-    let isLive = (env === 'staging' || env === 'production')
+  // let buffer
+  // let env = process.env.ARC_ENV || process.env.NODE_ENV
+  // let isLive = (env === 'staging' || env === 'production')
 
-    let exists = true
-    if (isLive) {
+  exists = true
+  if (isLive) {
     // read from s3
-      let Bucket = discovery['static']['bucket']
-      let Key = `${imagePath}`
-      try {
-        let result = await s3.getObject({ Bucket, Key, }).promise()
-        buffer = result.Body
-      }
-      catch (e) {
-        console.log(e)
-        exists = false
-      }
+    let Bucket = discovery['static']['bucket']
+    let Key = `${imagePath}`
+    try {
+      let result = await s3.getObject({ Bucket, Key, }).promise()
+      buffer = result.Body
     }
-    else {
+    catch (e) {
+      console.log(e)
+      exists = false
+    }
+  }
+  else {
     // read from local filesystem
-      let pathToStatic = path.join(__dirname, '../../../public' )
-      let pathToFile = path.join(pathToStatic, imagePath)
-      try {
-        buffer = fs.readFileSync(pathToFile)
-      }
-      catch (e){
-        console.log(e)
-        exists = false
-      }
+    // let pathToStatic = path.join(__dirname, '../../../public' )
+    let pathToStatic = localStaticDir
+    let pathToFile = path.join(pathToStatic, imagePath)
+    try {
+      buffer = fs.readFileSync(pathToFile)
     }
+    catch (e){
+      console.log(e)
+      exists = false
+    }
+  }
 
 
-    // 2. transform it
-    if (exists){
-      let Key = `${queryFingerprint}.${ext}`
+  // 2. transform it
+  if (exists){
+    let Key = `${queryFingerprint}.${ext}`
 
-      let size = sizeOf(buffer)
-      let scaling = Math.min(width / size.width, height / size.height)
-      let newWidth  = scaling * size.width
-      let newHeight = scaling * size.height
+    let size = sizeOf(buffer)
+    let scaling = Math.min(width / size.width, height / size.height)
+    let newWidth  = scaling * size.width
+    let newHeight = scaling * size.height
 
-      let image = await Jimp.read(buffer)
-      image.resize(newWidth, newHeight)
-      let output = await image.getBufferAsync(Jimp.AUTO)
-      if (isLive) {
-        let cacheBucket = discovery['storage-private']['image-cache']
-        await s3.putObject({
-          ContentType: mime,
-          Bucket: cacheBucket,
-          Key,
-          Body: output,
-        }).promise()
-      }
-      else {
-        fs.mkdirSync('../../../tmp-cache', { recursive: true })
-        fs.writeFileSync(path.resolve('../../../tmp-cache', Key), output)
-      }
-
-      // 4. respond with the image
-      return {
-        statusCode: 200,
-        headers: { 'content-type': mime, isBase64Encoded: true },
-        body: output.toString('base64')
-      }
-
+    let image = await Jimp.read(buffer)
+    image.resize(newWidth, newHeight)
+    let output = await image.getBufferAsync(Jimp.AUTO)
+    if (isLive) {
+      // let cacheBucket = discovery['storage-private']['image-cache']
+      await s3.putObject({
+        ContentType: mime,
+        Bucket: cacheBucket,
+        Key,
+        Body: output,
+      }).promise()
     }
     else {
-      return { statusCode: 404 }
+      // fs.mkdirSync('../../../tmp-cache', { recursive: true })
+      // fs.writeFileSync(path.resolve('../../../tmp-cache/', Key), output)
+      fs.writeFileSync(path.resolve(cacheBucket, Key), output)
     }
+
+    // 4. respond with the image
+    return {
+      statusCode: 200,
+      headers: { 'content-type': mime, isBase64Encoded: true },
+      body: output.toString('base64')
+    }
+
+  }
+  else {
+    return { statusCode: 404 }
   }
 
 }
 
-module.exports = { package }
+module.exports = { package, sandbox, variables,  imageHandler }
